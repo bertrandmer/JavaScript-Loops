93c50a74cc7e4109da571fc1b8afdc9e58d4ea45
bertrandmer <97544521+bertrandmer@users.noreply.github.com>
2022-02-09 15:15:10 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+console.log("Hello World!\n==========\n");
+
+// Exercise 1 Section
+console.log("EXERCISE 1:\n==========\n");
+
+for (let numb = 1; num <= 100; num++) {
+    if (num % 2 == 1) {
+        console.log(num);
+    }
+}
+
+
+
+
+
+// Exercise 2 Section
+console.log("EXERCISE 2:\n==========\n");
+for (let i = 1; i <= 100; i++) {
+   if (i % 3 == 0){
+    console.log(`${i} FIZZ`);
+   } else if (i % 5 == 0){
+       console.log(`${i} BUZZ`)
+   } else if (i % 3 == 0 && i % 5 == 0){
+       console.log(`${i} FIZZBUZZ`)
+   }
+}
